9e21ef3da49ddd392e7052dbf62dd729f288a272
Lawson Woods <ljwoods2@asu.edu>
2024-01-26 14:38:25 -0700

    Format comparisons

--- zarrtraj/__init__.py
+++ zarrtraj/__init__.py
@@ -5,5 +5,6 @@ This is a kit that provides the ability to read and write trajectory data in the
 
 # Add imports here
 from importlib.metadata import version
+from .ZARRTRAJ import *
 
 __version__ = version("zarrtraj")
--- zarrtraj/playground.py
+++ zarrtraj/playground.py
@@ -1,22 +1,11 @@
-from Zarr import ZarrReader
+
 import MDAnalysis as mda
 import zarr
 import numpy as np
+import os
+import h5py
 
+print(os.getcwd())
 
-# Create an initialized array (change np.zeros to np.ones if you want ones)
-data = np.zeros([3, 3])
-
-# Open the Zarr file for writing
-a = zarr.open_array(path='/home/law/workspace/zarrtraj/all_ones.zarr', mode='w-', shape=data.shape, dtype=data.dtype)
-
-# Assign data to the Zarr array
-a[:] = data
-
-# Print details
-print(a)
-print(a.shape)
-
-# Load the array to check
-loaded = zarr.load('/home/law/workspace/zarrtraj/all_ones.zarr')
-print(loaded)
+z = zarr.open_group('zarrtraj/10e3_zarr_c.zarrtraj', 'r')
+print(z)
