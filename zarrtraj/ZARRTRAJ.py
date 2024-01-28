0874a7f936bdb55ca04ec8882369d65e05baff80
Lawson Woods <ljwoods2@asu.edu>
2024-01-28 14:39:13 -0700

    Reader development

--- zarrtraj/ZARRTRAJ.py
+++ zarrtraj/ZARRTRAJ.py
@@ -29,14 +29,14 @@ class ZarrTrajReader(base.ReaderBase):
 
     @store_init_arguments
     def __init__(self, filename,
-                 convert_units=True,
                  **kwargs):
         
         if not HAS_ZARR:
-            raise RuntimeError("Please install h5py")
+            raise RuntimeError("Please install zarr")
         super(ZarrTrajReader, self).__init__(filename, **kwargs)
         self.filename = filename
-        self.convert_units = convert_units # NOTE: Not yet implemented
+        # NOTE: Not yet implemented
+        # self.convert_units = convert_units 
 
         self.open_trajectory()
         if self._particle_group['box'].attrs['dimension'] != 3:
@@ -46,7 +46,7 @@ class ZarrTrajReader(base.ReaderBase):
         # _has dictionary used for checking whether zarrtraj file has
         # 'position', 'velocity', or 'force' groups in the file
         self._has = {name: name in self._particle_group for
-                     name in ('position', 'velocity', 'force')} # NOTE: _has not yet implemented
+                     name in ('position', 'velocity', 'force')} 
         
         # Gets some info about what settings the datasets were created with
         # from first available group
@@ -54,14 +54,12 @@ class ZarrTrajReader(base.ReaderBase):
             if value:
                 dset = self._particle_group[f'{name}/value']
                 self.n_atoms = dset.shape[1]
-                self.compression = dset.compression
-                self.compression_opts = dset.compression_opts
+                self.compressor = dset.compressor
                 break
         else:
             raise NoDataError("Provide at least a position, velocity"
                               " or force group in the h5md file.")
         
-        # NOTE: _Timestep not yet implemented
         self.ts = self._Timestep(self.n_atoms,
                                  positions=self.has_positions,
                                  velocities=self.has_velocities,
@@ -72,8 +70,8 @@ class ZarrTrajReader(base.ReaderBase):
                       'length': None,
                       'velocity': None,
                       'force': None}
-        self._set_translated_units()  # fills units dictionary NOTE not yet implemented
-        self._read_next_timestep() # NOTE: Not yet implemented
+        # self._set_translated_units()  # fills units dictionary NOTE not yet implemented
+        self._read_next_timestep() 
 
     @staticmethod
     def _format_hint(thing):
@@ -109,6 +107,7 @@ class ZarrTrajReader(base.ReaderBase):
      
     @staticmethod
     def parse_n_atoms(filename):
+        # NOTE: This may fail if the filename passed is already a zarr group
         with zarr.open_group(filename, 'r') as f:
             for group in f['particles/trajectory']:
                 if group in ('position', 'velocity', 'force'):
@@ -145,7 +144,7 @@ class ZarrTrajReader(base.ReaderBase):
                     break
             else:
                 raise NoDataError("Provide at least a position, velocity"
-                                  " or force group in the h5md file.")
+                                  " or force group in the zarrtraj file.")
         except (ValueError, IndexError):
             raise IOError from None
 
@@ -160,7 +159,7 @@ class ZarrTrajReader(base.ReaderBase):
         self._copy_to_data()
 
         # Sets frame box dimensions
-        # Note: H5MD files must contain 'box' group in each 'particles' group
+        # Note: Zarrtraj files must contain 'box' group in each 'particles' group
         if "edges" in particle_group["box"]:
             edges = particle_group["box/edges/value"][frame, :]
             # A D-dimensional vector or a D × D matrix, depending on the
@@ -184,10 +183,87 @@ class ZarrTrajReader(base.ReaderBase):
         if self._has['force']:
             self._read_dataset_into_ts('force', ts.forces)
 
-        if self.convert_units:
-            self._convert_units()
+        # NOTE: Not sure about unit conversions yet
+        #if self.convert_units:
+        #    self._convert_units()
 
         return ts
+    
+    def _copy_to_data(self):
+        """assigns values to keys in data dictionary"""
+
+        # pulls 'time' and 'step' out of first available parent group
+        for name, value in self._has.items():
+            if value:
+                if 'time' in self._particle_group[name]:
+                    self.ts.time = self._particle_group[name][
+                        'time'][self._frame]
+                    break
+        for name, value in self._has.items():
+            if value:
+                if 'step' in self._particle_group[name]:
+                    self.ts.data['step'] = self._particle_group[name][
+                        'step'][self._frame]
+                    break
+
+    def _read_dataset_into_ts(self, dataset, attribute):
+        """reads position, velocity, or force dataset array at current frame
+        into corresponding ts attribute"""
+
+        n_atoms_now = self._particle_group[f'{dataset}/value'][
+                                           self._frame].shape[0]
+        if n_atoms_now != self.n_atoms:
+            raise ValueError(f"Frame {self._frame} of the {dataset} dataset"
+                             f" has {n_atoms_now} atoms but the initial frame"
+                             " of either the postion, velocity, or force"
+                             f" dataset had {self.n_atoms} atoms."
+                             " MDAnalysis is unable to deal"
+                             " with variable topology!")
+
+        self._particle_group[f'{dataset}/value'].read_direct(
+                             attribute, source_sel=np.s_[self._frame, :])
+    
+    def _read_next_timestep(self):
+        """read next frame in trajectory"""
+        return self._read_frame(self._frame + 1)
+    
+    @property
+    def has_positions(self):
+        """``True`` if 'position' group is in trajectory."""
+        return self._has['position']
+
+    @has_positions.setter
+    def has_positions(self, value: bool):
+        self._has['position'] = value
+
+    @property
+    def has_velocities(self):
+        """``True`` if 'velocity' group is in trajectory."""
+        return self._has['velocity']
+
+    @has_velocities.setter
+    def has_velocities(self, value: bool):
+        self._has['velocity'] = value
+
+    @property
+    def has_forces(self):
+        """``True`` if 'force' group is in trajectory."""
+        return self._has['force']
+
+    @has_forces.setter
+    def has_forces(self, value: bool):
+        self._has['force'] = value
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        del state['_particle_group']
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__ = state
+        self._particle_group = self._file['particles'][
+                               list(self._file['particles'])[0]]
+        self[self.ts.frame]
 
 class ZarrTrajWriter(base.WriterBase):
     format = 'ZARRTRAJ'
