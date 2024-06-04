7f1af792b475b834a9a00f7202899705498eddac
ljwoods2 <ljwoods2@asu.edu>
2024-06-04 07:39:56 -0700

    testfiles

--- zarrtraj/tmp.py
+++ zarrtraj/tmp.py
@@ -3,29 +3,16 @@ import kerchunk.hdf
 import ujson
 import xarray as xr
 import os
+import zarr
 
 os.environ["S3_REGION_NAME"] = "us-west-1"
 os.environ["AWS_PROFILE"] = "sample_profile"
 
 so = dict(anon=False, default_fill_cache=False, default_cache_type="first")
 url = "s3://zarrtraj-test-data/peg.h5md"
-# storage_options = dict(default_fill_cache=False, default_cache_type="none")
-#
-#
-# with fsspec.open(url, **storage_options) as inf:
-#    ref = kerchunk.hdf.SingleHdf5ToZarr(
-#        inf, url, inline_threshold=0
-#    ).translate()
-#    # now that we have this .zmetadata key, we
-#    # need to actually write it to the h5md file
-#    with open("reference.json", "w") as f:
-#        json.dump(ref, f)
-#
-#
 
 with fsspec.open(url, **so) as inf:
     h5chunks = kerchunk.hdf.SingleHdf5ToZarr(inf, url, inline_threshold=100)
-    h5chunks.translate()
     with open("single_file_kerchunk.json", "wb") as f:
         f.write(ujson.dumps(h5chunks.translate()).encode())
 
@@ -37,8 +24,7 @@ fs = fsspec.filesystem(
     remote_options=dict(anon=False),
     skip_instance_cache=True,
 )
-# m = fsspec.get_mapper("reference://", fo="reference.json", remote_protocol="s3")
-ds = xr.open_datatree(
-    fs.get_mapper(""), engine="zarr", backend_kwargs={"consolidated": False}
-)
-print(ds)
+
+z = zarr.open_group(fs.get_mapper(""), mode="r")
+
+print(z.tree())
