fe85db03754af459cc3c9d2e01429fd3c63bce3d
Lawson Woods <ljwoods2@asu.edu>
2024-07-13 16:21:54 +0000

    bug fixes

--- benchmarks/reader_bms.py
+++ benchmarks/reader_bms.py
@@ -6,10 +6,32 @@ import MDAnalysis.analysis.rms as rms
 from MDAnalysis.coordinates.H5MD import H5MDReader
 import zarr
 import h5py
-import dask as da
+import dask.array as da
 
 import os
 
+from dask.distributed import Client, LocalCluster
+
+
+"""
+1. Activate the devtools/asv_env.yaml environment
+
+2. Make sure to set the BENCHMARK_DATA_DIR to wherever local yiip files are stored
+
+3. To run, use:
+
+Development:
+
+    asv run -q -v -e benchmarks-dev > bm.log &
+
+Full run:
+
+    asv run -v -e benchmarks-dev > bm.log &
+
+4. To publish, use
+
+
+"""
 
 BENCHMARK_DATA_DIR = os.getenv("BENCHMARK_DATA_DIR")
 os.environ["S3_REGION_NAME"] = "us-west-1"
@@ -34,6 +56,11 @@ h5md_files = [
     f"{BENCHMARK_DATA_DIR}/yiip_aligned_uncompressed.h5md",
 ]
 
+s3_zarrmd_files = [
+    "s3://zarrtraj-test-data/yiip_aligned_compressed.zarrmd",
+    "s3://zarrtraj-test-data/yiip_aligned_uncompressed.zarrmd",
+]
+
 
 def dask_rmsf(positions):
     mean_positions = positions.mean(axis=0)
@@ -49,6 +76,7 @@ class ZARRH5MDDiskStrideTime(object):
 
     params = local_files
     param_names = ["filename"]
+    timeout = 2400.0
 
     def setup(self, filename):
         self.reader_object = ZARRH5MDReader(filename)
@@ -57,6 +85,9 @@ class ZARRH5MDDiskStrideTime(object):
         """Benchmark striding over full trajectory"""
         for ts in self.reader_object:
             pass
+
+    def teardown(self, filename):
+        del self.reader_object
 
 
 class ZARRH5MDS3StrideTime(object):
@@ -64,6 +95,7 @@ class ZARRH5MDS3StrideTime(object):
 
     params = s3_files
     param_names = ["filename"]
+    timeout = 2400.0
 
     def setup(self, filename):
         self.reader_object = ZARRH5MDReader(filename)
@@ -73,12 +105,16 @@ class ZARRH5MDS3StrideTime(object):
         for ts in self.reader_object:
             pass
 
+    def teardown(self, filename):
+        del self.reader_object
+
 
 class H5MDReadersDiskStrideTime(object):
     """Benchmarks for zarrmd and h5md file striding using local files."""
 
     params = (h5md_files, [ZARRH5MDReader, H5MDReader])
     param_names = ["filename", "reader"]
+    timeout = 2400.0
 
     def setup(self, filename, reader):
         self.reader_object = reader(filename)
@@ -88,26 +124,23 @@ class H5MDReadersDiskStrideTime(object):
         for ts in self.reader_object:
             pass
 
+    def teardown(self, filename, reader):
+        del self.reader_object
 
-class H5MDFmtDiskRSMFTime(object):
-    """Benchmarks for zarrtraj file striding."""
+
+class H5MDFmtDiskRMSFTime(object):
 
     params = (local_files, ["dask", "mda"])
     param_names = ["filename", "method"]
+    timeout = 2400.0
 
     def setup(self, filename, method):
         if method == "dask":
-            if filename.endswith(".h5md"):
-                self.positions = da.from_array(
-                    h5py.File(filename)["/particles/trajectory/position/value"]
-                )
-
-            elif filename.endswith("zarrmd"):
-                self.positions = da.from_array(
-                    zarr.open_group(filename)[
-                        "/particles/trajectory/position/value"
-                    ]
-                )
+            self.positions = da.from_array(
+                zarr.open_group(filename)[
+                    "/particles/trajectory/position/value"
+                ]
+            )
 
         elif method == "mda":
             self.universe = mda.Universe(
@@ -123,26 +156,26 @@ class H5MDFmtDiskRSMFTime(object):
             rmsf = dask_rmsf(self.positions)
             rmsf.compute()
 
+    def teardown(self, filename, method):
+        if hasattr(self, "positions"):
+            del self.positions
+        if hasattr(self, "universe"):
+            del self.universe
 
-class H5MDFmtAWSRSMFTime(object):
-    """Benchmarks for zarrtraj file striding."""
 
-    params = (s3_files, ["dask", "mda"])
+class H5MDFmtAWSRMSFTime(object):
+
+    params = (s3_zarrmd_files, ["dask", "mda"])
     param_names = ["filename", "method"]
+    timeout = 2400.0
 
     def setup(self, filename, method):
         if method == "dask":
-            if filename.endswith(".h5md"):
-                self.positions = da.from_array(
-                    h5py.File(filename)["/particles/trajectory/position/value"]
-                )
-
-            elif filename.endswith(".zarrmd"):
-                self.positions = da.from_array(
-                    zarr.open_group(filename)[
-                        "/particles/trajectory/position/value"
-                    ]
-                )
+            self.positions = da.from_array(
+                zarr.open_group(filename)[
+                    "/particles/trajectory/position/value"
+                ]
+            )
 
         elif method == "mda":
             self.universe = mda.Universe(
@@ -153,7 +186,13 @@ class H5MDFmtAWSRSMFTime(object):
     def time_rmsf(self, filename, method):
         """Benchmark striding over full trajectory"""
         if method == "mda":
-            rms.RMSF(self.universe.atoms)
+            rms.RMSF(self.universe.atoms).run()
         elif method == "dask":
             rmsf = dask_rmsf(self.positions)
             rmsf.compute()
+
+    def teardown(self, filename, method):
+        if hasattr(self, "positions"):
+            del self.positions
+        if hasattr(self, "universe"):
+            del self.universe
